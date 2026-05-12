d8e180d2441221ba08a3044956069c4e61ece343
Matheus Silva <matheuswnscf@gmail.com>
2026-05-12 15:44:44 -0300

    Melhorar estrutura do arquivo txt de log. Melhorar estrutura do arquivo de embeddings.

--- src/clean_data.py
+++ src/clean_data.py
@@ -22,7 +22,9 @@ def log(message:str,file,print_log:bool=False):
 # Função principal, lida com leitura do arquivo, limpeza e escrita do arquivo final
 def main():
     
-    log("\n\n\nLendo arquivo de notícias brutas", log_file,True)
+    print("\n"*3)
+    log("Lendo arquivo de notícias brutas", log_file,True)
+    
     with open("./dados/noticias_brutas.json", "r", encoding="utf-8") as f:
         noticias_brutas = json.load(f)
         
@@ -30,12 +32,17 @@ def main():
     
     for noticia in noticias_brutas:
         log(f"----Processando notícia: {noticia['id']}----", log_file)
+        
         dirty_text = noticia['texto']
         cleaned_text = clean_text(dirty_text,noticia['data'])
 
         if len(cleaned_text) > 20:
             noticia['texto'] = cleaned_text
             noticias_validas.append(noticia)
+            
+        log(f"----Notícia processada: {noticia['id']}----\n\n\n", log_file)
+            
+            
             
     log("Escrevendo arquivo de notícias limpas\n\n\n", log_file,True)
     with open("./dados/noticias_limpa.json", "w", encoding="utf-8") as f:
@@ -45,20 +52,33 @@ def main():
 # ' ' & '\n'
 def clean_more_one_space_and_breaklines(dirty_text:str):
     cleaned_text = ' '.join(dirty_text.split())
+    difference = get_str_difference(dirty_text, cleaned_text)
+    
+    
+    log("# Limpando mais de um espaço e quebras de linha dos textos", log_file)
+    log(f"### Mais de um espaço e quebras de linha removidos: {difference} caracteres", log_file)
     return cleaned_text
 
 # Função para limpar tags HTML usando BeautifulSoup
 # O método get_text() extrai o texto, removendo as tags HTML
 def clean_html_tags(dirty_text:str):
-    log("Limpando tags HTML dos textos", log_file)
     soup = BeautifulSoup(dirty_text, "html.parser")
     cleaned_text = soup.get_text()
+    difference = get_str_difference(dirty_text, cleaned_text)
+    
+    
+    log("# Limpando tags HTML dos textos", log_file)
+    log(f"### Tags HTML removidas: {difference} caracteres", log_file)
     return cleaned_text
 
 # Função para limpar uma palavra específica do texto
 def clean_per_word(dirty_text:str, word:str):    
-    log(f"Limpando palavras dos textos: {word}", log_file)
     cleaned_text = dirty_text.replace(word, '')
+    difference = get_str_difference(dirty_text, cleaned_text)
+    
+    
+    log(f"# Limpando palavras dos textos: {word}", log_file)
+    log(f"### Palavras removidas: {difference} caracteres", log_file)
     return cleaned_text
 
 # Função para limpar a data do texto, formatando a data no objeto no formato YYYY-MM-DD
@@ -66,22 +86,34 @@ def clean_per_word(dirty_text:str, word:str):
 def clean_per_date(dirty_text:str, date:str):
     date = datetime.strptime(date, "%Y-%m-%d")
     date = date.strftime("%d/%m/%Y")
-    log(f"Limpando data dos textos: {date}", log_file)
     cleaned_text = dirty_text.replace(date, '')
+    difference = get_str_difference(dirty_text, cleaned_text)
+    
+    
+    log(f"# Limpando data dos textos: {date}", log_file)
+    log(f"### Datas removidas: {difference} caracteres", log_file)
     return cleaned_text
 
 # Função para limpar horas do texto, usando regex para encontrar padrões 
 # de horas
 def clean_per_hour(dirty_text:str):
     hour_pattern = r'(?i)(às\s*)?([0-1]?[0-9]|2[0-3])h[0-5][0-9]'
-    log("Limpando horas dos textos", log_file)
     cleaned_text = re.sub(hour_pattern, '', dirty_text)
+    difference = get_str_difference(dirty_text, cleaned_text)
+    
+    
+    log(f"# Limpando horas dos textos", log_file)
+    log(f"### Horas removidas: {difference} caracteres", log_file)
     return cleaned_text
 
 # Função para limpar caracteres especiais específicos do texto, como '-', '|', '—'
 def clear_per_special_characters(dirty_text:str,char:str):
-    log(f"Limpando caracteres especiais dos textos: {char}", log_file)
     cleaned_text = dirty_text.replace(f' {char} ' ,' ')
+    difference = get_str_difference(dirty_text, cleaned_text)
+    
+    
+    log(f"# Limpando caracteres especiais dos textos: {char}", log_file)
+    log(f"### Caracteres '{char}' removidos: {difference} caracteres", log_file)
     return cleaned_text
 
 # Função para limpar o texto, chamando as funções de limpeza em sequência
@@ -95,6 +127,9 @@ def clean_text(dirty_text:str,date_to_clear:str):
     cleaned_text = clear_per_special_characters(cleaned_text, '—')
     cleaned_text = clean_more_one_space_and_breaklines(cleaned_text)
     return cleaned_text
+
+def get_str_difference(str1:str, str2:str):
+    return len(str1) - len(str2)
 
 if __name__ == "__main__":
     main()
--- src/exec_embeddings.py
+++ src/exec_embeddings.py
@@ -2,45 +2,104 @@ import json
 from sentence_transformers import SentenceTransformer
 import torch
 
+queries = ["mudanças na taxa de juros",
+    "mercado de trabalho e desemprego",
+    "inflação e preços ao consumidor",]
 
+embedders = [
+    SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2'),
+    SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+]
+embedders_names = [
+    "paraphrase-multilingual-mpnet-base-v2",
+    "paraphrase-multilingual-MiniLM-L12-v2"
+]
+
+with open("./dados/noticias_limpa.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+    
+# Res format
+# query: { model: { corpus_texts: {id: int, score: float} } }
+def create_empty_res_dict(queries:list[str], embedders_names:list[str]):
+    res = {}
+    for query in queries:
+        res[query] = {}
+        for name in embedders_names:
+            res[query][name] = {
+                "sem título": {"id": [], "score": []},
+                "com título": {"id": [], "score": []}
+            }
+    return res
 
 def main():
-    embedder = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
+    res = create_empty_res_dict(queries, embedders_names)
+    corpus_texts, corpus_texts_with_title = get_corpus_texts(data)
+        
+    for embedder, name in zip(embedders, embedders_names):
+        
+        corpus_embeddings = embedder.encode(corpus_texts, convert_to_tensor=True)
+        corpus_embeddings_with_title = embedder.encode(corpus_texts_with_title, convert_to_tensor=True)
+        
+        for query in queries:
+            
+            id_top_news_1 = []
+            id_top_news_2 = []
+            
+            print(f"\n--- Query: '{query}' | Modelo: {name} ---")
+            query_embedding = embedder.encode(query, convert_to_tensor=True)
+            scores, indices = get_scores(embedder, query_embedding, corpus_embeddings)
+
+            print(f"Resultados usando {name}: (sem título)")
+            for score, index in zip(scores, indices):
+                print(f"Score: {score:.4f} - Notícia: {data[index]['titulo']}")
+                id_top_news_1.append(data[index]['id'])
+                res[query][name]["sem título"]["id"].append(data[index]['id'])
+                res[query][name]["sem título"]["score"].append(score.data.item())
+            
+            scores_with_title, indices_with_title = get_scores(embedder, query_embedding, corpus_embeddings_with_title)
+            
+            print(f"\nResultados usando {name}: (com título)")
+            for score, index in zip(scores_with_title, indices_with_title):
+                print(f"Score: {score:.4f} - Notícia: {data[index]['titulo']}")
+                id_top_news_2.append(data[index]['id'])
+                res[query][name]["com título"]["id"].append(data[index]['id'])
+                res[query][name]["com título"]["score"].append(score.data.item())
+            
+            difference = get_array_difference(id_top_news_1, id_top_news_2) + get_array_difference(id_top_news_2, id_top_news_1)
+            print(f"Id das notícias melhor ranqueadas sem título: {id_top_news_1}")
+            print(f"Id das notícias melhor ranqueadas com título: {id_top_news_2}")
+            print(f"Notícias sairam/entraram no rankeamento: {difference}")
     
-    with open("./dados/noticias_limpa.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
+    with open("./results.json", "w", encoding="utf-8") as f:
+        json.dump(res, f, ensure_ascii=False, indent=5)
+
+def get_corpus_texts(data:list[dict]):
     
     corpus_texts: list[str] = []
+    corpus_texts_with_title: list[str] = []
     
     for list_item in data:
         title: str = list_item['titulo']
         text:str = list_item['texto']
         unified_text = f"{title}. {text}"
-        corpus_texts.append(unified_text.lower())
         
-    corpus_embeddings = embedder.encode(corpus_texts, convert_to_tensor=True)
-    
-    queries = ["mudanças na taxa de juros",
-    "mercado de trabalho e desemprego",
-    "inflação e preços ao consumidor",]
-    
-    top_k = min(5, len(corpus_texts))
-    
-    # Etapa 3: Motor de Busca Semântica
-    for query in queries:
-        query_embedding = embedder.encode(query, convert_to_tensor=True)
+        corpus_texts.append(text.lower())
+        corpus_texts_with_title.append(unified_text.lower())
         
-        similarity_scores = embedder.similarity(query_embedding, corpus_embeddings)[0]
-        scores, indices = torch.topk(similarity_scores, k=top_k)
         
-        print("\nQuery:", query)
-        print("\nTop 5 notícias mais similares:")
+    return corpus_texts, corpus_texts_with_title
 
-        for score, idx in zip(scores, indices):
-            finded_new = data[idx]
-            print(f"  - (Score: {score:.4f}) Título: {finded_new['titulo']}")
-            
-        print("\n" + "="*50 + "\n")
+    
+def get_scores(model, query_embedding, corpus_embeddings, top_k:int=5):
+    
+    
+    similarity_scores = model.similarity(query_embedding, corpus_embeddings)[0]
+    scores, indices = torch.topk(similarity_scores, k=top_k)
+    
+    return scores, indices
+
+def get_array_difference(arr1:list, arr2:list):
+    return list(set(arr1) - set(arr2))
 
 if __name__ == "__main__":
     main()
